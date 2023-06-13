6cf891011633ac2e01af19450516793957c78309
JavierHornos <hjavih@gmail.com>
2023-06-13 11:26:51 -0300

    logo

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
         <Routes>
           <Route path='/' element={<ItemListContainer greeting="Productos" />} />
           <Route path='/categoria/:idCategoria' element={<ItemListContainer greeting="Productos" />} />
-          <Route path='/categoria/*' element={<h2 className='titulo'>El producto no existe </h2>} />
+          <Route path='/categoria/*' element={<h2 className='titulo'>La categoria no existe </h2>} />
           <Route path='/item/:idItem' element={<ItemDetailContainer/>} />
           <Route path='/item/*' element={<h2 className='titulo'>El producto no existe </h2>} />
           <Route path='/cart' element={<Cart/>} />
